089c87f68eca2c9ab34e6aa241ef46139b0a583c
jorismenge <mengejoris@gmail.com>
2025-02-18 23:37:10 +0100

    Update funktionen2.R
    Weitere Helferfunktion

--- funktionen2.R
+++ funktionen2.R
@@ -15,3 +15,29 @@ gini <- function(x){
   
   return(g)
 }
+
+# Helferfunktion berechnet Cohen's d
+cohens_d <- function(gruppe_1, gruppe_2) {
+  
+  pooled_sd <- sqrt((var(gruppe_1, na.rm = TRUE) + var(gruppe_2, na.rm = TRUE)) / 2)
+  
+  differenz_mittelwerte <- mean(gruppe_1, na.rm = TRUE) - mean(gruppe_2, na.rm = TRUE)
+  
+  cohen <- differenz_mittelwerte / pooled_sd
+  
+  return(cohen)
+}
+
+
+
+# Helferfunktion, die Dichotomie überprüft
+
+dichotomie <- function(variable) {
+  unique_values <- unique(variable)
+  
+  if (length(unique_values) != 2) {
+    stop("Die Variable muss genau zwei Kategorien haben.")
+  }
+  
+  return(TRUE)
+}
